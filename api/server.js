e2aabd0d2829ac67a771ab4b080324c81abd4cb7
drewmrodriguez <drewmrodriguez@gmail.com>
2021-08-18 20:58:57 -0500

    file structure set up, need to fix server listening on undefined port

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// const AuthRouter = require("./auth/auth-router");
+// const PlantRouter = require("./plants/plant-router");
+// const SpeciesRouter = require("./species/species-router");
+// const UserRouter = require("./users/users-routers");
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+//Sets up the router
+// server.use("/api/auth", AuthRouter);
+// server.use("/api/users", PlantRouter);
+// server.use("/api/items", SpeciesRouter);
+// server.use("/api/reviews", UserRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: process.env.MOTD });
+});
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+
+module.exports = server;
