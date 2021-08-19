ac1dcd253c85d74181e8b471a2c59dc594d25dea
drewmrodriguez <drewmrodriguez@gmail.com>
2021-08-18 21:21:12 -0500

    ran npx gitignore node

--- api/server.js
+++ api/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+// const AuthRouter = require("./auth/auth-router");
+// const PlantRouter = require("./plants/plant-router");
+// const SpeciesRouter = require("./species/species-router");
+// const UserRouter = require("./users/users-routers");
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
+//Sets up the router
+// server.use("/api/auth", AuthRouter);
+// server.use("/api/users", PlantRouter);
+// server.use("/api/species", SpeciesRouter);
+// server.use("/api/reviews", UserRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: process.env.MOTD });
